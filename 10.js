6a7a2d95de372cba8ac126785e151405ed84a690
Antonio Matheus <matheus69969@gmail.com>
2025-02-09 14:32:32 -0300

    Adicionando arquivos JS

---
+++ 10.js
@@ -0,0 +1,8 @@
+let a=Number(prompt('Digite o primeiro nuemero'));
+let b=Number(prompt('Digite um segundo numero'));
+function media(a,b){
+   return (a+b)/2;
+}
+
+const resultado=(media(a,b));
+console.log(`A média é igual a ${resultado}`);
